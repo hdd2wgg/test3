5fe677b51bccf87b4dc4d83555db384241cea1ac
hdd2wgg <72405574+hdd2wgg@users.noreply.github.com>
2020-10-06 18:23:55 +0800

    第四次测试

---
+++ test3/src/com/yunke/test3/Demo.java
@@ -0,0 +1,14 @@
+package com.yunke.test3;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+		for (int i = 0; i <= 100;i ++) {
+			System.out.println("Äã¸ö´óÉµ±Æ");
+		}
+	}
+
+}
